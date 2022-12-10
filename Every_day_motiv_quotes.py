fa21ee294e91b6181f342cd51c6523d27259517e
Hitaishkg <hitaishkg@gmail.com>
2022-12-11 01:35:46 +0530

    made function and runs in headless

--- Every_day_motiv_quotes.py
+++ Every_day_motiv_quotes.py
@@ -1,12 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 
 import time
+opts = Options()
+opts.headless = True
+assert opts.headless
 
-driver=Chrome(executable_path="C:\\bin\\chromedriver.exe")
-driver.get('https://quotesondesign.com/gonxalo-blanco/')
-driver.find_element("id", "get-another-quote-button").click()
-driver.implicitly_wait(90000)
-quote = driver.find_element("id","quote-content").text
-print(quote)
-driver.close()
+driver=Chrome(executable_path="C:\\bin\\chromedriver.exe",options=opts)
+
+def quote():
+    driver.get('https://quotesondesign.com/gonxalo-blanco/')
+    driver.find_element("id", "get-another-quote-button").click()
+    driver.implicitly_wait(9000000)
+    quote = driver.find_element("id","quote-content").text
+    driver.close()
+    return quote
+
+def main():
+    motiquote = quote()
+    print(motiquote)
+    quit()
+
+
+if __name__ == '__main__':
+    main()
